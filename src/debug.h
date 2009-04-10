d7acc0c29505202b9cb790a95bb286fcf3551f50
Patryk Cisek <patryk@prezu.one.pl>
2009-04-10 11:10:31 +0200

    Removed crufty warnings from src/debug.h

--- src/debug.h
+++ src/debug.h
@@ -1,12 +1,9 @@
 #include "pcproxy-config.h"
 
-#warning 1
 #ifdef DEBUG
-#warning 2
 #include <cstdio>
 
 #define pDebug(format, ...) printf("%s, %d: "format, __PRETTY_FUNCTION__, __LINE__, __VA_ARGS__)
 #else // DEBUG
 #define pDebug(format, ...)
 #endif // DEBUG
-#warning 3
