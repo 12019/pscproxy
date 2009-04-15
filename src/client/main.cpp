c844b5c6bb9542c7ea737fe0f0f617f4a6a5e45e
Patryk Cisek <patryk@prezu.one.pl>
2009-04-15 18:45:04 +0200

    Added signal handling to client

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <csignal>
+#include <execinfo.h>
+#include <cstdlib>
 
 #ifdef HAVE_GNUTLS
 #include <gnutls/gnutls.h>
@@ -13,14 +16,45 @@ GCRY_THREAD_OPTION_PTHREAD_IMPL;
 #include "season.h"
 #include "client_socket.h"
 
+static void PSProxySignalHandler(int signal) {
+#define SIZE 100
+	std::cerr << "Caught " << signal << " signal!!" << std::endl;
+	if(SIGSEGV == signal || SIGINT == signal || SIGTERM == signal) {
+		int i, numOfBacktraceStrings;
+		void *buf[SIZE];
+		char **strings;
+		numOfBacktraceStrings = backtrace(buf, SIZE);
+		std::cerr << "backtrace (" << numOfBacktraceStrings << " addresses):" << std::endl;
+		strings = backtrace_symbols(buf, numOfBacktraceStrings);
+		for(i = 0; i < numOfBacktraceStrings; ++i) {
+			std::cerr << strings[i] << std::endl;
+		}
+
+		abort();
+	}
+}
+
+void enableSignalHandling() {
+	signal(SIGSEGV, PSProxySignalHandler);
+	signal(SIGINT,  PSProxySignalHandler);
+	signal(SIGTERM, PSProxySignalHandler);
+	signal(SIGUSR1, PSProxySignalHandler);
+	signal(SIGPIPE, SIG_IGN);
+
+}
+
 int main(int argc, char *argv[]) {
 #ifdef HAVE_GNUTLS
 	gcry_control (GCRYCTL_SET_THREAD_CBS, &gcry_threads_pthread);
 	gnutls_global_init();
 #endif // HAVE_GNUTLS
+	enableSignalHandling();
 
 	std::cout << "Hello world!!" << std::endl;
 	PSProxy::ClientSocket socket("127.0.0.1", 10000);
+	do {
+	} while(1);
+
 	return 0;
 
 	PSProxy::Season season(PSProxy::SeasonConfig("~/.psproxy-client.conf"));
